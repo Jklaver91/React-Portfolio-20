8d72bcf415ecbd4076f738cfb33aa5756b79038e
Justin Klaver <Klaverts@yahoo.com>
2022-11-25 12:33:09 -0500

    project content added

--- react-portfolio/src/components/pages/Home.js
+++ react-portfolio/src/components/pages/Home.js
@@ -3,6 +3,8 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
+      <h1>Home Page</h1>
+        <h2> Welcome to my page!</h2>
     </div>
   );
 }
--- react-portfolio/src/components/pages/Project.js
+++ react-portfolio/src/components/pages/Project.js
@@ -3,7 +3,25 @@ import React from 'react';
 export default function Project() {
   return (
     <div>
-     
+      <h1>Project Page</h1>
+      <ul>
+        <li>
+          <a href="https://github.com/Jklaver91/Challenge1" target="_blank" rel="noreferrer">
+          Horiseon Challenge</a>
+        </li>
+        <li>
+          <a href="https://pdismore.github.io/Date-Night-Streaming-Generator/" target="_blank" rel="noreferrer">
+          Streaming Search</a>
+        </li>
+        <li>
+          <a href="https://qc-foodies.herokuapp.com/" target="_blank" rel="noreferrer">
+          QC Foodies!</a>
+        </li>
+        <li>
+          <a href="https://www.google.com/" target="_blank" rel="noreferrer">
+          Untrue Facts</a>
+        </li>
+      </ul>
     </div>
   );
 }
