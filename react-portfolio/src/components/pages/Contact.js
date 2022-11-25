abea8136ab6ac995f27c43d468e4a234e73a8076
Justin Klaver <Klaverts@yahoo.com>
2022-11-25 12:33:36 -0500

    contact content added

--- react-portfolio/src/components/pages/Contact.js
+++ react-portfolio/src/components/pages/Contact.js
@@ -3,6 +3,25 @@ import React from 'react';
 export default function Contact() {
   return (
     <div>
+      <h1>Contact Page</h1>
+      <ul>
+        <li>
+          <a href="https://github.com/Jklaver91" target="_blank" rel="noreferrer">
+            Github</a>
+        </li>
+        <li>
+          <a href="mailto:klaverts@yahoo.com" target="_blank" rel="noreferrer">
+            Email</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/justin-klaver-a74376239/" target="_blank" rel="noreferrer">
+            linkedin</a>
+        </li>
+        <li>
+          <a href="https://voice.google.com/u/0/about" target="_blank" rel="noreferrer">
+            Phone: (555)-555-5555</a>
+        </li>
+      </ul>
     </div>
   );
 }
