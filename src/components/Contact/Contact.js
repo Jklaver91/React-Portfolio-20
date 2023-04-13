0dbc35f3a224685f2123e9c75abdac1aa8ed3736
Justin Klaver <Klaverts@yahoo.com>
2023-04-12 22:13:21 -0400

    updated portfolio

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,53 +1,7 @@
-import React, { useState } from 'react';
-import './style.css';
+import React from 'react';
 import { Card } from 'react-bootstrap';
 
-// Here we import a helper function that will check if the email is valid
-import { validateEmail } from '../../utils/helpers';
-
-function Contact() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
-  const [email, setEmail] = useState('');
-  const [userName, setUserName] = useState('');
-  const [message, setMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
-    const { target } = e;
-    const inputType = target.name;
-    const inputValue = target.value;
-
-    // Based on the input type, we set the state of either email, username, and password
-    if (inputType === 'email') {
-      setEmail(inputValue);
-    } else if (inputType === 'userName') {
-      setUserName(inputValue);
-    } else{
-      setMessage(inputValue);
-    }
-  };
-
-  const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    e.preventDefault();
-
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
-    if (!validateEmail(email) || !userName) {
-      setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
-      return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
-    }
-    alert(`Thank you for the message ${userName}`);
-
-    // If everything goes according to plan, we want to clear out the input after a successful registration.
-    setUserName('');
-    setEmail('');
-    setMessage('');
-  };
-
+export default function Contact() {
   return (
     <div>
       <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{ color: "#0c3f63" }}>
@@ -56,42 +10,9 @@ function Contact() {
           <Card.Text></Card.Text>
         </Card.Body>
       </Card>
-
-
-    <div className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{ color: "#0c3f63" }}>
-      <form className="form">
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          type="email"
-          placeholder="email"
-        />
-        <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="username"
-        />
-
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          type="text"
-          placeholder="message"
-        />
-        <button type="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {errorMessage && (
-        <div>
-          <p className="error-text">{errorMessage}</p>
-        </div>
-      )}
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeUvpPMULxzksPA9W_Cv_bLQlJx0i0KrPDgf0fBtoOFVHt03w/viewform?embedded=true" width="640" height="1690" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </div>
     </div>
   );
 }
-
-export default Contact;
--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import project1 from '../images/evenmorecode200.jpg';
-import project2 from '../images/deskpc200.jpg';
-import project3 from '../images/circuit200.jpg';
-import project4 from '../images/morecode200.jpg';
+import React, { useState } from 'react';
+import { Card, Col, Row, Modal  } from 'react-bootstrap';
+import project1 from '../images/radiantCauldron.png';
+import project2 from '../images/streamSearch.png';
+import project3 from '../images/qcFoodies.png';
+import project4 from '../images/cuttingEdge.png';
 
 export default function Project() {
+  const [showModal, setShowModal] = useState(false);
+  const [modalImageSrc, setModalImageSrc] = useState('');
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalText, setModalText] = useState('');
+  const [modalLink, setModalLink] = useState('');
+
+  const openModal = (src, title, text, link) => {
+    setModalImageSrc(src);
+    setModalTitle(title);
+    setModalText(text);
+    setModalLink(link);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setModalImageSrc('');
+    setModalTitle('');
+    setModalText('');
+    setModalLink('');
+    setShowModal(false);
+  };
+
+
   return (
-    <div>
+    <div className='mb-5'>
       <div>
       <Card className='mx-auto mb-3 bg-primary text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
         <Card.Body>
@@ -16,64 +39,79 @@ export default function Project() {
       </Card>
       </div>
 
-    <div className='container'>
-      <section className='row justify-content-around'>
-      <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
-        <Card.Img className='w-50 p-3' src={project1} alt={'generic tech photo'} /> 
-        <Card.Body>
-          <Card.Title className='text-center'><a className='text-white' href="https://github.com/Jklaver91/Challenge1" target="_blank" rel="noreferrer">
-          Horiseon Challenge</a></Card.Title>
-          <Card.Text>My first work in the class refactoring HTML. A humble beginning. 
-         </Card.Text>
-        </Card.Body>
-      </Card>
+      <div className='container'>
+        <section className='row justify-content-around'>
 
-      <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
-      <Card.Img className='w-50 p-3' src={project2} alt={'generic tech photo'} /> 
-        <Card.Body>
-          <Card.Title className='text-center'><a className='text-white' href="https://pdismore.github.io/Date-Night-Streaming-Generator/" target="_blank" rel="noreferrer">
-          Streaming Search</a></Card.Title>
-          <Card.Text> My first group work within the course. A tool to look up where any title you want is streaming, providing links as well.</Card.Text>
-        </Card.Body>
-      </Card>
-      
-      <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
-      <Card.Img className='w-50 p-3' src={project3} alt={'generic tech photo'} /> 
-        <Card.Body>
-          <Card.Title className='text-center'><a className='text-white' href="https://qc-foodies.herokuapp.com/" target="_blank" rel="noreferrer">
-          QC Foodies!</a></Card.Title>
-          <Card.Text> My second group work within the course. A simple blog to post, comment, and upvote posts about local restaurants. This project saw my implimentaion of Cloudinary services to provide cloud based image upload to the site.</Card.Text>
-        </Card.Body>
-      </Card>
+          <Row className='row-cols-1 row-cols-md-2 g-4'>
+          <Col>
+              <Card className="bg-primary" onClick={() => openModal(project1, 'Radiant Cauldron', 'A simple informational page for a local businesses events. This includes the ability to email the owner, sign up to their vendor lists with google, as well as view their upcoming events and links to their community pages.', 'https://github.com/Jklaver91/Challenge1')}>
+                <Card.Img className="w-50 p-3 mx-auto" src={project1} alt={'generic tech photo'} />
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    <a className="text-white" href="https://github.com/Jklaver91/Challenge1" target="_blank" rel="noreferrer">
+                      Radiant Cauldron
+                    </a>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
 
-      <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
-      <Card.Img className='w-50 p-3' src={project4} alt={'generic tech photo'} /> 
-        <Card.Body>
-          <Card.Title className='text-center'><a className='text-white' href="https://cutting-edge-pro-max.herokuapp.com/" target="_blank" rel="noreferrer">
-          Cutting-Edge-Pro</a></Card.Title>
-          <Card.Text>My third group work within the course. A online storefront to have your images etched, engraved, or cut our of a multitude of materials. This project saw my implimentaion of Cloudinary services to provide cloud based image upload to the site, as well as a group effort on front and back end development.</Card.Text>
-        </Card.Body>
-      </Card>
+            <Col>
+              <Card className="bg-primary" onClick={() => openModal(project2, 'Streaming Search', 'My first group work within the course. A tool to look up where any title you want is streaming, providing links as well.', 'https://pdismore.github.io/Date-Night-Streaming-Generator/')}>
+                <Card.Img className="w-50 p-3 mx-auto" src={project2} alt={'generic tech photo'} />
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    <a className="text-white" href="https://pdismore.github.io/Date-Night-Streaming-Generator/" target="_blank" rel="noreferrer">
+                      Streaming Search
+                    </a>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
 
-      <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
-      <Card.Img className='w-50 p-3' src={project4} alt={'generic tech photo'} /> 
-        <Card.Body>
-          <Card.Title className='text-center'><a className='text-white' href="https://www.google.com/" target="_blank" rel="noreferrer">
-          Another project</a></Card.Title>
-          <Card.Text> This will be where a fifth project worth sharing will be placed.</Card.Text>
-        </Card.Body>
-      </Card>
+            <Col>
+              <Card className="bg-primary" onClick={() => openModal(project3, 'QC Foodies', 'My second group work within the course. A simple blog to post, comment, and upvote posts about local restaurants. This project saw my implimentaion of Cloudinary services to provide cloud based image upload to the site.', 'https://qc-foodies.herokuapp.com/')}>
+                <Card.Img className="w-50 p-3 mx-auto" src={project3} alt={'generic tech photo'} />
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    <a className="text-white" href="https://pdismore.github.io/Date-Night-Streaming-Generator/" target="_blank" rel="noreferrer">
+                      QC Foodies!
+                    </a>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
 
-      <Card className='mx-auto mb-3 bg-primary inline-block text-white h-100 d-flex align-items-center justify-content-center col-3' style={{color: "#0c3f63" }}>
-      <Card.Img className='w-50 p-3' src={project4} alt={'generic tech photo'} /> 
-        <Card.Body>
-          <Card.Title className='text-center'><a className='text-white' href="https://www.google.com/" target="_blank" rel="noreferrer">
-          Lastly One more project</a></Card.Title>
-          <Card.Text> This will be where a sixth project worth sharing will be placed.</Card.Text>
-        </Card.Body>
-      </Card>
+            <Col>
+              <Card className="bg-primary" onClick={() => openModal(project4, 'Cutting Edge Pro', 'My third group work within the course. A online storefront to have your images etched, engraved, or cut our of a multitude of materials. This project saw my implimentaion of Cloudinary services to provide cloud based image upload to the site, as well as stripe payment implimentaion and group effort on front and back end development. *Note* the site is currently down due the cost of upkeep the link will take you to a youtube demonstration', 'https://www.youtube.com/watch?v=eRLPcZxiDgo')}>
+                <Card.Img className="w-50 p-3 mx-auto" src={project4} alt={'generic tech photo'} />
+                <Card.Body>
+                  <Card.Title className="text-center">
+                    <a className="text-white" href="https://pdismore.github.io/Date-Night-Streaming-Generator/" target="_blank" rel="noreferrer">
+                      Cutting Edge Pro
+                    </a>
+                  </Card.Title>
+                </Card.Body>
+              </Card>
+            </Col>
+    </Row>
       </section>
       </div>
+
+      <Modal show={showModal} onHide={closeModal} size="xl">
+        <Modal.Header closeButton>
+          <Modal.Title>{modalTitle}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={modalImageSrc} className="w-100" alt="project" />
+          <p>{modalText}</p>
+          <p>
+            <a href={modalLink} target="_blank" rel="noreferrer">
+              Visit project repository
+            </a>
+          </p>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
